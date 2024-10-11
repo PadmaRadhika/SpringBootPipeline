b0d604c3e216395fa60872bcee7091ba172a8b51
PadmaRadhika <radhika.piratla@gmail.com>
2024-10-10 21:15:10 -0700

    added jenkins file

---
+++ src/main/java/com/example/webcontentdemo/WebcontentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webcontentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcontentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcontentdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webcontentdemo/WebcontentdemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.webcontentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class WebcontentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	void simpleTest() {
+		assertTrue(true);
+	}
+
+}
